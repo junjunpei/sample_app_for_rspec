02866075ccedfff90287ee147ac949507b76dac2
大村純平 <junpei.aug07@gmail.com>
2021-01-24 16:29:50 +0900

    Fix: .valid?をexpect().to be_invalidに変更

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,20 +10,20 @@ RSpec.describe Task, type: :model do
 
     it 'is invalid without title' do
       task_without_title = FactoryBot.build(:task, title: nil)
-      task.valid?
-      expect(task.errors[:title]).to include("can't be blank")
+      expect(task_without_title).to be_invalid
+      expect(task_without_title.errors[:title]).to include("can't be blank")
     end
 
     it 'is invalid without status' do
       task_without_status = FactoryBot.build(:task, status: nil)
-      task.valid?
-      expect(task.errors[:status]).to include("can't be blank")
+      expect(task_without_status).to be_invalid
+      expect(task_without_status.errors[:status]).to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
       task = FactoryBot.create(:task)
       task_with_duplicated_title = FactoryBot.build(:task, title: task.title)
-      task_with_duplicated_title.valid?
+      expect(task_with_duplicated_title).to be_invalid
       expect(task_with_duplicated_title.errors[:title]).to include("has already been taken")
     end
 
