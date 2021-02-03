85c05a7164e1a4edad28aa90cb1b97e30033ce45
大村純平 <junpei.aug07@gmail.com>
2021-02-03 15:15:12 +0900

    Fix: ログインのシステムスペック

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "UserSessions", type: :system do
       it 'ログイン処理が失敗する' do
         visit login_path
         fill_in 'Email', with: nil
-        fill_in 'Password', with: nil
+        fill_in 'Password', with: 'foobar'
         click_button 'Login'
         expect(current_path).to eq login_path
         expect(page).to have_content 'Login failed'
